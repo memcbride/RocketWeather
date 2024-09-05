4125d9a5a174abf4694bc016c0f0a368324f94b2
MacModeler <memcbride@mac.com>
2024-09-05 12:23:33 -0400

    Initial Commit

---
+++ RocketWeather/RocketWeatherApp.swift
@@ -0,0 +1,20 @@
+// ------------------------------------------------------
+// Project: RocketWeather
+// File: RocketWeatherApp.swift
+// Created by Mark McBride on 2024.09.05
+// Last Updated:  2024.09.05
+// GitHub: https://github.com/memcbride
+// ------------------------------------------------------
+// Copyright © 2024 by MacModeler.  All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct RocketWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
